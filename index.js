c74b98db279aa782fbcc77922b5dd5fdc4ef4081
Clinton Robinson <clint@usful.co>
2015-08-14 12:06:08 -0400

    Adding updateMany

--- index.js
+++ index.js
@@ -138,6 +138,28 @@ Mongo.prototype.findOneAndUpdate = function(collection, query, update, options) 
   });
 };
 
+Mongo.prototype.updateMany = function(collection, query, update, options) {
+  var self = this;
+
+  options = options || {};
+
+  return new Promise(function(resolve, reject) {
+    function go() {
+      self.db.collection(collection).updateMany(query, update, options, function(err, doc) {
+        err
+          ? reject(err)
+          : resolve(doc)
+        ;
+      });
+    }
+
+    self.db
+      ? go()
+      : self.connect().then(go).catch(reject)
+    ;
+  });
+};
+
 Mongo.prototype.count = function(collection, query, options) {
   var self = this;
 
