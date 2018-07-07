4b0246a40df34a08ca9255c24378efe2502d44d0
Clinton Robinson <clinton.robinson@gmail.com>
2018-07-07 19:02:50 -0400

    Refactoring for mongodb 3.x, this library is essentially pointless now

--- index.js
+++ index.js
@@ -1,254 +1,99 @@
-"use strict";
+const MongoCursor = require('mongodb').Cursor;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-var Cursor = require('./Cursor');
+class Mongo {
+  constructor({ host = 'localhost', port = '27017', db = 'dev', url }) {
+    //If a URL wasn't provided, create one
+    this.dbName = db;
 
-//Expose the library
-exports = module.exports = Mongo;
+    if (!url) {
+      this.url = `mongodb://${host}:${port}/${db}`;
+    }
+  }
 
-Mongo.Cursor = Cursor;
-Mongo.ObjectID = ObjectID;
+  static get Cursor() {
+    return MongoCursor;
+  }
 
-function Mongo(config) {
-  if (!(this instanceof Mongo)) return new Mongo(config);
+  static get ObjectID() {
+    return ObjectID;
+  }
 
-  this.config = config || {host: 'localhost', port: '27017', db: 'dev'};
+  async connect() {
+    if (this.db) {
+      return this.db;
+    }
 
-  //If a URL wasn't provided, create one
-  if (!this.config.url) {
-    this.config.url = 'mongodb://' + this.config.host + ':' + this.config.port + '/' + this.config.db;
+    this.client = await MongoClient.connect(this.url);
+
+    this.db = this.client.db(this.dbName);
+
+    return this.db;
+  }
+
+  async find(collection, query, options = {}) {
+    await this.connect();
+
+    return await this.db.collection(collection).find(query, options);
+  }
+
+  async findOne(collection, query, options = {}) {
+    await this.connect();
+
+    return await this.db.collection(collection).findOne(query, options);
+  }
+
+  async findOneAndDelete(collection, query, options = {}) {
+    await this.connect();
+
+    return await this.db
+      .collection(collection)
+      .findOneAndDelete(query, options);
+  }
+
+  async findOneAndUpdate(collection, query, update, options = {}) {
+    await this.connect();
+
+    return await this.db
+      .collection(collection)
+      .findOneAndUpdate(query, update, options);
+  }
+
+  async remove(collection, query, options = {}) {
+    await this.connect();
+    return await this.db.collection(collection).remove(query, options);
+  }
+
+  async updateMany(collection, query, update, options = {}) {
+    await this.connect();
+    return await this.db
+      .collection(collection)
+      .updateMany(query, update, options);
+  }
+
+  async count(collection, query, options = {}) {
+    await this.connect();
+    return await this.db.collection(collection).count(query, options);
+  }
+
+  async insert(collection, doc) {
+    await this.connect();
+
+    const isArray = Array.isArray(doc);
+
+    const result = await this.db
+      .collection(collection)
+      [isArray ? 'insertMany' : 'insertOne'](doc);
+
+    return isArray ? result.ops : result.ops[0];
+  }
+
+  async createIndexes(collection, indexes) {
+    await this.connect();
+
+    return this.db.collection(collection).createIndexes(indexes);
   }
 }
 
-Mongo.prototype.connect = function() {
-  var self = this;
-
-  return new Promise(function(resolve, reject) {
-    if (self.db) {
-      resolve(self.db);
-      return;
-    }
-
-    MongoClient.connect(self.config.url, function(err, db) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      self.db = db;
-
-      resolve(self.db);
-    });
-  });
-};
-
-/**
- * Returns a Promise that resolves an Iterable
- * @param collection
- * @param query
- * @returns {Promise}
- */
-Mongo.prototype.find = function(collection, query, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).find(query, options, function(err, cursor) {
-        err ?
-          reject(err) :
-          resolve(new Cursor(cursor))
-        ;
-      });
-    }
-
-    self.db ?
-      go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.findOne = function(collection, query, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).findOne(query, options, function(err, doc) {
-
-        err
-          ? reject(err)
-          : resolve(doc)
-        ;
-      });
-    }
-
-    self.db
-      ? go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.findOneAndDelete = function(collection, query, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).findOneAndDelete(query, options, function(err, doc) {
-        err
-          ? reject(err)
-          : resolve(doc)
-        ;
-      });
-    }
-
-    self.db
-      ? go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.findOneAndUpdate = function(collection, query, update, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).findOneAndUpdate(query, update, options, function(err, doc) {
-        err
-          ? reject(err)
-          : resolve(doc)
-        ;
-      });
-    }
-
-    self.db
-      ? go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-
-Mongo.prototype.remove = function(collection, query, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).remove(query, options, function(err, doc) {
-        err
-          ? reject(err)
-          : resolve(doc)
-        ;
-      });
-    }
-
-    self.db
-      ? go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.updateMany = function(collection, query, update, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).updateMany(query, update, options, function(err, doc) {
-        err
-          ? reject(err)
-          : resolve(doc)
-        ;
-      });
-    }
-
-    self.db
-      ? go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.count = function(collection, query, options) {
-  var self = this;
-
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).count(query, options, function(err, count) {
-        err ?
-          reject(err) :
-          resolve(count)
-        ;
-      });
-    }
-
-    self.db ?
-      go()
-      : self.connect().then(go).catch(reject)
-    ;
-  });
-};
-
-Mongo.prototype.insert = function(collection, doc) {
-  var self = this;
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      var func = Array.isArray(doc) ? 'insertMany' : 'insertOne';
-
-      self.db.collection(collection)[func](doc, function(err, result) {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        if (func === 'insertMany') {
-          resolve(result.ops);
-        } else {
-          resolve(result.ops[0]);
-        }
-      });
-    }
-
-    self.db ?
-      go()
-      : self.connect().then(go).catch(reject);
-  });
-};
-
-Mongo.prototype.createIndexes = function(collection, indexes) {
-  var self = this;
-
-  return new Promise(function(resolve, reject) {
-    function go() {
-      self.db.collection(collection).createIndexes(indexes, function(err, result) {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        resolve(result);
-      })
-    }
-
-    self.db ?
-      go()
-      : self.connect().then(go).catch(reject);
-  });
-};
+exports = module.exports = Mongo;
